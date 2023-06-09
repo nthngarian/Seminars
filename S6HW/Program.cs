6d0f4f6230ce3cd0edc9e4c38c2f5356d371e9b1
Alexei <nthngarian@gmail.com>
2023-06-09 21:59:24 +0300

    changed folder S7

--- S6HW/Program.cs
+++ S6HW/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿/*
+int length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[length]; // length = 5, 5 нулей
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент: "); // i = 0
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество положительных элементов: {count}");
+*/
+/*
+int length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[length]; // length = 5, 5 нулей
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Массив: [ {string.Join(";", numbers)} ]");
+Console.WriteLine($"Количество положительных элементов: {count}");
+*/
+/*
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double axisX = (b2 - b1) / (k1 - k2);
+double axisY = k1 * axisX + b1;
+
+Console.WriteLine($"Точка пересечения двух прямых = X: {Math.Ceiling(axisX)}, Y: {Math.Ceiling(axisY)}");
+*/
--- S7/Program.cs
+++ S7/Program.cs
@@ -1,1 +1,162 @@
-﻿
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+/*
+// Двумерный массив
+
+// метод для создания массива 
+// Двумерный массив = matrix
+// m - кол-во строчек, n - столбцов
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество cтолбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resMatrix = GetMatrix(rows, columns, 0, 10);
+// rows=3, columns=4 => таблица из 3 строк и 4 столбцов, элемент: число от 0 до 10 включительно
+PrintMatrix(resMatrix); // PrintMatrix(матрица)
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n]; // Таблица из m строк и n столбцов 
+    for (int i = 0; i < matrix.GetLength(0); i++) // Цикл по строчкам , i < m
+    {
+        // i,j,m,k 
+        for (int j = 0; j < matrix.GetLength(1); j++)// Цикл по стобцам , j < n
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+// Метод, который печатает массив 
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // строчки
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // столбцы
+        {
+            Console.Write(array[i,j] + "\t"); // "\t" = Tab = 4 пробела между элементами
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+/*
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество cтолбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resMatrix = GetMatrix(rows, columns, 0, 10);
+// rows=3, columns=4 => таблица из 3 строк и 4 столбцов, элемент: число от 0 до 10 включительно
+PrintMatrix(resMatrix); // PrintMatrix(матрица)
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n]; // Таблица из m строк и n столбцов 
+    for (int i = 0; i < matrix.GetLength(0); i++) // Цикл по строчкам , i < m
+    {
+        // i,j,m,k 
+        for (int j = 0; j < matrix.GetLength(1); j++)// Цикл по стобцам , j < n
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+// Метод, который печатает массив 
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // строчки
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // столбцы
+        {
+            Console.Write(array[i,j] + "\t"); // "\t" = Tab = 4 пробела между элементами
+        }
+        Console.WriteLine();
+    }
+}
+*/
+
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+/*
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество cтолбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resMatrix = GetMatrix(rows, columns, 0, 10);
+// rows=3, columns=4 => таблица из 3 строк и 4 столбцов, элемент: число от 0 до 10 включительно
+PrintMatrix(resMatrix); // PrintMatrix(матрица)
+ReplaceByEvenIndex(resMatrix); 
+Console.WriteLine();
+PrintMatrix(resMatrix); 
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n]; // Таблица из m строк и n столбцов 
+    for (int i = 0; i < matrix.GetLength(0); i++) // Цикл по строчкам , i < m
+    {
+        // i,j,m,k 
+        for (int j = 0; j < matrix.GetLength(1); j++)// Цикл по стобцам , j < n
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // строчки
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // столбцы
+        {
+            Console.Write(array[i,j] + "\t"); // "\t" = Tab = 4 пробела между элементами
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplaceByEvenIndex(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // строчки
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // столбцы
+        {
+            if (i % 2 == 0 && i > 0 && j % 2 == 0 && j > 0 ) 
+            {
+             array[i, j] = (int)Math.Pow(array[i, j], 2);   
+            }
+        }
+        
+    }
+}
+*/
