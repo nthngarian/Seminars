f57f0cd07c2e2a23673ee6ffad344ea2fd8cafd2
Alexei <nthngarian@gmail.com>
2023-06-12 19:54:45 +0300

    changed folder S6HW

--- S6HW/Program.cs
+++ S6HW/Program.cs
@@ -1,44 +1,35 @@
-﻿/*
-int length = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[length]; // length = 5, 5 нулей
-for (int i = 0; i < numbers.Length; i++)
-{
-    Console.WriteLine($"Введите {i + 1} элемент: "); // i = 0
-    numbers[i] = Convert.ToInt32(Console.ReadLine());
-}
-int count = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > 0)
-    {
-        count++;
-    }
-}
-Console.WriteLine($"Количество положительных элементов: {count}");
-*/
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
 /*
+Console.Write("Введите размер массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[length]; // length = 5, 5 нулей
 int count = 0;
 for (int i = 0; i < numbers.Length; i++)
 {
+    Console.Write($"Введите {i + 1} элемент: ");
     numbers[i] = Convert.ToInt32(Console.ReadLine());
     if (numbers[i] > 0)
     {
         count++;
     }
 }
-Console.WriteLine($"Массив: [ {string.Join(";", numbers)} ]");
+Console.WriteLine($"Массив: [{string.Join("; ", numbers)}]");
 Console.WriteLine($"Количество положительных элементов: {count}");
 */
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 /*
-Console.WriteLine("введите значение b1");
+Console.Write("Введите значение b1: ");
 double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k1");
+Console.Write("Введите число k1: ");
 double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите значение b2");
+Console.Write("Введите значение b2: ");
 double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k2");
+Console.Write("Введите число k2: ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
 double axisX = (b2 - b1) / (k1 - k2);
